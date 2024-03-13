a66b909d2f6add153ccef997b3e064cae9ee8a5f
fborgazzi <b.fiona.03@gmail.com>
2024-03-13 14:19:18 +0100

    creation de l'inteface

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr6/QuizzGenius/services/interfaces/IQuestionnaire.java
@@ -0,0 +1,16 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.services.interfaces;
+
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.entities.dto.QuestionDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.entities.dto.QuestionnaireDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.utiles.Exceptions.LienException;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.utiles.Exceptions.NoStat;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.utiles.Exceptions.NumTropGrandException;
+
+public interface IQuestionnaire {
+
+    public QuestionnaireDTO chargerListeQuest(String path) throws LienException;
+    public QuestionDTO chargerUneQuestion(int idQuestion) throws NumTropGrandException;
+    public String majStatQuestion() throws NoStat;
+
+    public String fournirStatsQuestion()throws NoStat;
+}
