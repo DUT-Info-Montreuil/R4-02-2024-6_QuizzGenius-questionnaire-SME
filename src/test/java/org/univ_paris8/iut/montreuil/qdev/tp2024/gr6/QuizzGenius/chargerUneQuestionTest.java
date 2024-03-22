2254a1416661bb81eeac75857539de0111f0647b
fborgazzi <b.fiona.03@gmail.com>
2024-03-22 13:18:40 +0100

    merge

--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr6/QuizzGenius/chargerUneQuestionTest.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr6/QuizzGenius/chargerUneQuestionTest.java
@@ -3,6 +3,7 @@ package org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius;
 import org.junit.jupiter.api.Test;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.entities.dto.QuestionDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.entities.dto.QuestionnaireDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.services.impl.QuestionImpl;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.services.interfaces.IServiceQuestionnaire;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.utiles.Exceptions.LienException;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGeniusMockTest.IServiceQuestionnaireChargerListeQuestMock1;
@@ -14,8 +15,9 @@ import org.univ_paris8.iut.montreuil.qdev.tp2024.gr6.QuizzGenius.utiles.Exceptio
 import static org.junit.jupiter.api.Assertions.*;
 
 class chargerUneQuestionTest {
-    IServiceQuestionnaire servQuest = new IServiceQuestionnaireChargerListeQuestMock1();
-    QuestionnaireDTO questions2 =  servQuest.chargerListeQuest("src/main/ressources/questionsQuizz_V1.1.csv");
+    IServiceQuestionnaire servQuest = new QuestionImpl();
+    //IServiceQuestionnaire servQuest = new IServiceQuestionnaireChargerListeQuestMock1();
+    QuestionnaireDTO questions2 =  servQuest.chargerListeQuest("/home/etudiants/info/fborgazzi/IdeaProjects/R4-02-2024-6_QuizzGenius-questionnaire-SME/src/main/resources/questionsQuizz_V1.1.csv");
 
     chargerUneQuestionTest() throws LienException {
     }
@@ -24,17 +26,17 @@ class chargerUneQuestionTest {
     //IQuestionnaire iQuestionnaire;
     @Test
     void questionCorrecte() throws NumTropGrandException {
-        IServiceQuestionnaireChargerUneQuestionEqual iQuestionnaire = new IServiceQuestionnaireChargerUneQuestionEqual();
-        QuestionDTO laQuestion = iQuestionnaire.chargerUneQuestion(1,1,questions2);
+       // IServiceQuestionnaireChargerUneQuestionEqual iQuestionnaire = new IServiceQuestionnaireChargerUneQuestionEqual();
+        QuestionDTO laQuestion = servQuest.chargerUneQuestion(1,1,questions2);
         QuestionDTO leTest = new QuestionDTO(1, 1, Langue.fr, "De quel petit objet se munit le golfeur pour surélever sa balle avant de la frapper ?", "Tee", 1, "Le joueur peut poser sa balle sur une cheville de bois ou de plastique qui ne peut pas être utilisée en dehors des départs.", "https://fr.wikipedia.org/wiki/Matériel_de_golf");
         assertEquals(laQuestion, leTest);
     }
 
     @Test
     void testException () throws NumTropGrandException {
-        IServiceQuestionnaireChargerUneQuestExp iQuestionnaire = new IServiceQuestionnaireChargerUneQuestExp();
+        //IServiceQuestionnaireChargerUneQuestExp iQuestionnaire = new IServiceQuestionnaireChargerUneQuestExp();
         assertThrows(NumTropGrandException.class, () -> {
-            iQuestionnaire.chargerUneQuestion(31,1,questions2);
+            servQuest.chargerUneQuestion(31,1,questions2);
         });
     }
 
